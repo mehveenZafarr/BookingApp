549b8fb3ebc9d273b3ab4812d6863537f82d3709
mehveenzafar <121675157+mehveenzafar@users.noreply.github.com>
2024-08-30 11:55:45 +0500

    header changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,14 +5,14 @@ import { UserContext } from './UserContext.jsx'
 const Header = () => {
     const { user } = useContext(UserContext);
     return (
-        <header className='flex justify-between'>
+        <header className='flex justify-between bg-gray-100 p-4 rounded-2xl'>
             {/* logo */}
             <Link to={'/'} className="flex items-center gap-1">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8 -rotate-90">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#FF0000" className="size-8 -rotate-90">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
                 </svg>
                 <span className='font-bold text-xl'>
-                    airbnb
+                    minibnb
                 </span>
             </Link>
             {/* search */}
